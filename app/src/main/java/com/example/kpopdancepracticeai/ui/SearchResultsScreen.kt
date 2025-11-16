8415b9350d057cd37fedcd23acfcae9e4a1e7923
HyeonU-Seol <hyeonws0705@gmail.com>
2025-11-16 10:34:06 +0900

    edit

--- app/src/main/java/com/example/kpopdancepracticeai/ui/SearchResultsScreen.kt
+++ app/src/main/java/com/example/kpopdancepracticeai/ui/SearchResultsScreen.kt
@@ -1,6 +1,5 @@
 package com.example.kpopdancepracticeai.ui
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -8,7 +7,6 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
